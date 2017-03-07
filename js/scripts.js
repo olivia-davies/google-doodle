05257ec216afc68841d0c7d567a9b1dcd976ec9a
Olivia Davies <olivia.davies@eagles.ewu.edu>
2017-03-06 19:40:53 -0800

    Start Over
    * project is late - Got behind in my work
    * Changed Songkran theme to Pusheen Cat

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,1 @@
 // YOUR SCRIPTS GO HERE
-var img0 = '../img/200.jpg';
-var img1 = '../img/300.jpg';
-var img2 = '../img/400.jpg';
-
-
-
-// Get Random Number
-var max = 3;
-var numRand = Math.floor(Math.random() * max);
-console.log(numRand);
-
-// Change class on body element
-$('body').addClass('bg' + numRand);
-
-// Display Randomized Message 
-var tmp = $('#random').attr('src', eval('img' + numRand));
