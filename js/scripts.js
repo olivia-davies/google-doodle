6a0a8d078d523fa93ff37589aa031bdafc804612
Olivia Davies <olivia.davies@eagles.ewu.edu>
2017-03-07 17:03:58 -0800

    UPDATE
    *added Pusheen cats
    *really worked on CSS and HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-// YOUR SCRIPTS GO HERE
+// YOUR SCRIPTS GO HERE//
+
+//DRAW SVG
+
 var mySVG = $('svg').drawsvg();
 
 mySVG.drawsvg('animate');
+
+
+
+
+
+
+
+// Store & Generate Number
+var maxRand = 6;
+var numRand = Math.floor(Math.random() * maxRand);
+//console.log(numRand);
+
+// Store images
+var img0 = 'img/cheetocat.png';
+var img1 = 'img/eyebrows.png';
+var img2 = 'img/gangnamstyle.png';
+var img3 = 'img/harrykitty.png';
+var img4 = 'img/ramencat.png';
+var img5 = 'img/sherlockcat.png';
+
+// Show Images
+$('img').attr('src', eval('img' + numRand));
